8dbe2d459404206ea8addbdb3e3a09086f0dab33
Timothée Jeannin <timojeajea@gmail.com>
2015-10-01 14:01:00 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('LOADED');
+$('*').each(function () {
+    if ($(this).css('background-color') != 'rgba(0, 0, 0, 0)') {
+        var elementColor = jQuery.Color($(this), 'background-color');
+        var elementNewColor = jQuery.Color(
+            300 - elementColor.red(),
+            300 - elementColor.green(),
+            300 - elementColor.blue(),
+            elementColor.alpha());
+        console.log($(this), elementColor, elementNewColor);
+        $(this).css('background-color', elementNewColor);
+    }
+});
